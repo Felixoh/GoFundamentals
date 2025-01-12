1efd47d077f29fa7d328a73f9ca483c3780190ba
Felixoh <devfeloh1@gmail.com>
2025-01-12 04:38:43 +0300

    new commits

---
+++ chapter3/Arraytest.go
@@ -0,0 +1,6 @@
+package chapter3
+
+func ReturnArray() [4]string {
+	Names := [4]string{"mochama", "samwel", "dianah", "grragher"}
+	return Names
+}
--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello golander ! welcome back")
+	//mes := [4]string{"kurt", "janis", "amy", "mochamas"}
+
+	//iterate over the array
+	//"for i := 0; i < len(names); i++ {
+	//fmt.Println(names[i])
+	//}
+
+	//use of the range keyword:
+	//
+
+	//use of map and initialisations
+	var users map[string]string
+
+	//initialising the array
+
+	users = make(map[string]string)
+
+	users["kurt@gmail.com"] = "mochama"
+	users["devfeloh1@gmail.com"] = "janisa"
+	users["jamii@2024"] = "jafrice"
+
+	for key, value := range users {
+		fmt.Printf("%s %s'\n", key, value)
+	}
+
 }
