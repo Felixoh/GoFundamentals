f2b293431c8c533904589d371a9e7da898cd0a43
Felixoh <devfeloh1@gmail.com>
2025-01-14 00:13:22 +0300

    new added file 13th jan

--- main.go
+++ main.go
@@ -1,31 +1,13 @@
 package main
 
-import "fmt"
+import "testing"
+
+func TestSimple(t *testing.T) {
+	if true {
+		t.Fatal("Expected false,got true")
+	}
+}
 
 func main() {
-	//mes := [4]string{"kurt", "janis", "amy", "mochamas"}
-
-	//iterate over the array
-	//"for i := 0; i < len(names); i++ {
-	//fmt.Println(names[i])
-	//}
-
-	//use of the range keyword:
-	//
-
-	//use of map and initialisations
-	var users map[string]string
-
-	//initialising the array
-
-	users = make(map[string]string)
-
-	users["kurt@gmail.com"] = "mochama"
-	users["devfeloh1@gmail.com"] = "janisa"
-	users["jamii@2024"] = "jafrice"
-
-	for key, value := range users {
-		fmt.Printf("%s %s'\n", key, value)
-	}
 
 }
